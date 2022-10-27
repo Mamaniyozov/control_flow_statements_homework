1b71732abba255fe6648315c5e36792c8c2e5a7f
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-10-27 07:33:20 +0500

    5m

--- if05.py
+++ if05.py
@@ -8,4 +8,18 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    if a<0 and b<0 and c<0:
+        return "manfiy sonlar 3 ta"
+    if a>0 and b<0 and c<0:
+        return "manfiy sonlar 2ta"
+    if a<0 and b>0 and c>0:
+        return "manfiy sonlar 1ta"
+    if a>0 and b>0 and c<0:
+        return "manfiy sonlar 1 ta"
+    if a>0 and b<0 and c>0:
+        return "manfiy sonlar 1ta"
+    if a>0 and b>0 and c>0:
+        return "musbat sonlar "
+    if a<0 and b>0 and c<0:
+        return "manfiy sonlar 2ta"
+print(main(-2,3,-2))
