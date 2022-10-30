56c0c83f96d576a5793e6eabfe02ef1e7c079ea6
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-10-30 20:19:26 +0500

    8-misol

--- if08.py
+++ if08.py
@@ -11,4 +11,18 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    a=323
+    s=""
+    if a<99 and a<1000:
+        s+="three digit"
+        if a%100%10%2==1:
+            return '3xonali toq son'
+    if a>9 and a<100:
+        s+="two digit"
+        if a%10%2==1:
+            return '2 xonali toq son'
+    
+    if a%10%2==0:
+        return '2xonali juft son '
+    if a%100%10%2==0:
+        return '3 xonali juft son'
